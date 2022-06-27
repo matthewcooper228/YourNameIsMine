cf151821d555b97678e0fde1acf5f7cfaa6d47cc
matthewcooper228 <matthewcooper228@gmail.com>
2022-06-27 14:19:31 -0700

    solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isSubsequence(string name1, string name2)
+{
+    if(name1.Length == name2.Length && name1 != name2)
+    {
+        return false;
+    }
+    string longName = "";
+    string shortName = "";
+    if(name1.Length < name2.Length)
+    {
+        longName = name2;
+        shortName = name1;
+    }
+    else
+    {
+        longName = name1;
+        shortName = name2;
+    }
+    for(int i = 0; i < shortName.Length; i++)
+    {
+        if(shortName[i] != longName[i])
+        {
+            longName = longName.Substring(0,i) + longName.Substring(i + 1, longName.Length - i - 1);
+        }
+    }
+    longName = longName.Substring(0, shortName.Length);
+    return shortName == longName;
+}
